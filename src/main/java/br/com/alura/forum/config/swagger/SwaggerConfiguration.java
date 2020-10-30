a8507f42bb932e38131a9dc2287cf9bf89a7cfab
rodrigo.resque <rodrigo.resque@tre-pa.jus.br>
2020-10-30 12:06:00 -0300

    updt

--- src/main/java/br/com/alura/forum/config/swagger/SwaggerConfiguration.java
+++ src/main/java/br/com/alura/forum/config/swagger/SwaggerConfiguration.java
@@ -6,10 +6,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import br.com.alura.forum.modelo.Usuario;
+import springfox.documentation.builders.ApiDescriptionBuilder;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -19,6 +23,7 @@ public class SwaggerConfiguration {
 	@Bean
 	public Docket forumApi() {
 		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("br.com.alura.forum"))
                 .paths(PathSelectors.ant("/**"))
@@ -33,6 +38,15 @@ public class SwaggerConfiguration {
                                     .parameterType("header")
                                     .required(false)
                                     .build()));
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Forum api rest service")
+				.description("Um simples servico rest for beneficar software")
+				.contact(new Contact("Rodrigo Resque teste", "http://localhost/exemplo", "rodrigo.resque@email.com"))
+				.version("1.0")
+				.build();
 	}
 	
 }
